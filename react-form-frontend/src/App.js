6b7c5e984e5aeaea70ab412bab99c29472b08329
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-10-27 23:02:54 +0530

    c

---
+++ react-form-frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import UserForm from "./components/UserForm";
+import Table from "./components/Table";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Navbar />
+        <Switch>
+          <Route path="/users" component={Table} />
+          <Route path="/" component={UserForm} />
+        </Switch>
+      </Router>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ react-form-frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+
+import React from "react"
+
+const Header = () => {
+    return (
+        <div className="container-fluid p-3">
+            <p className="h1 font-weight-bold text-center">React Form</p>
+        </div>
+    )
+}
+
+export default Header;
